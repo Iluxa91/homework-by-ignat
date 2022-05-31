13f1774045830ef3855a024265b98ceab352264b
Ilua Kisialiou <iluxa91@inbox.ru>
2022-05-31 21:34:13 +0300

    Commit for HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,24 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionType = { type: 'sort', payload: 'up' | 'down' } | { type: 'check', payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            let sortUp = (x:UserType, y:UserType) => {
+                if (x.name > y.name) return 1;
+                else if (x.name < y.name) return -1;
+                else return 0
+            }
+            let newArr = [...state].sort(sortUp)
+
+            return action.payload === 'up' ? newArr : newArr.reverse()
         }
         case 'check': {
             // need to fix
-            return state
+            return state.filter(el => el.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
