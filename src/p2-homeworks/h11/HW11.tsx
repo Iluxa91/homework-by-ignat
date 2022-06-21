9946e3dbdc601cdd137095937035ba0bedbb60c1
Ilua Kisialiou <iluxa91@inbox.ru>
2022-06-21 11:53:23 +0300

    Commit for HW11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,17 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from './common/c7-SuperRange/SuperRange.module.css'
 
-function HW11() {
+
+export function HW11() {
+
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
+
+    const onChangeRange = (value:number) => {
+        return setValue1(value)
+    }
 
     return (
         <div>
@@ -12,9 +19,11 @@ function HW11() {
             homeworks 11
 
             {/*should work (должно работать)*/}
-            <div>
+            <div className={s.superRangeContainer}>
                 <span>{value1}</span>
                 <SuperRange
+                    onChangeRange={onChangeRange}
+                    value={value1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
@@ -22,6 +31,11 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperDoubleRange
+                    min={0}
+                    max={100}
+                    arrValue={[value1,value2]}
+                    setMinVal={setValue1}
+                    setMaxVal={setValue2}
                     // сделать так чтоб value1 и value2 изменялось
                 />
                 <span>{value2}</span>
@@ -35,5 +49,3 @@ function HW11() {
         </div>
     )
 }
-
-export default HW11
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,86 @@
-import React from 'react'
+import React, {useLayoutEffect, useState} from 'react'
+import './SuperDoubleRange.css'
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+    arrValue: [number, number]
+    min: number
+    max: number
+    setMinVal: (value: number) => void
+    setMaxVal: (value: number) => void
+
     // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
+        onChangeRange, arrValue, min, max, setMinVal, setMaxVal
         // min, max, step, disable, ...
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+    let minVal = arrValue[0]
+    let maxVal = arrValue[1]
+    const [avg, setAvg] = useState((min + max) / 2);
+    let thumbsize = 14;
+    const width = 215;
+    const minWidth =
+        thumbsize + ((avg - min) / (max - min)) * (width - 2 * thumbsize);
+    const minPercent = ((minVal - min) / (avg - min)) * 100;
+    const maxPercent = (maxVal - avg) / (max - avg) * 100;
+    const styles = {
+        min: {
+            width: minWidth,
+            left: 20,
+            "--minRangePercent": `${minPercent}%`
+        },
+        max: {
+            width: thumbsize + ((max - avg) / (max - min)) * (width - 2 * thumbsize),
+            left: minWidth,
+            "--maxRangePercent": `${maxPercent}%`
+        }
+    };
+
+    useLayoutEffect(() => {
+        setAvg((maxVal + minVal) / 2);
+    }, [minVal, maxVal]);
 
     return (
-        <>
-            DoubleRange
-        </>
+        <div
+            className="min-max-slider"
+            data-legendnum="2"
+            data-rangemin={min}
+            data-rangemax={max}
+            data-thumbsize={thumbsize}
+            data-rangewidth={width}
+        >
+            <label htmlFor="min">Minimum price</label>
+            <input
+                id="min"
+                className="min"
+                style={styles.min}
+                name="min"
+                type="range"
+                step="1"
+                min={min}
+                max={avg}
+                value={minVal}
+                onChange={({ target }) => setMinVal(Number(target.value))}
+            />
+            <label htmlFor="max">Maximum price</label>
+            <input
+                id="max"
+                className="max"
+                style={styles.max}
+                name="max"
+                type="range"
+                step="1"
+                min={avg}
+                max={max}
+                value={maxVal}
+                onChange={({ target }) => setMaxVal(Number(target.value))}
+            />
+        </div>
+
     )
 }
 
